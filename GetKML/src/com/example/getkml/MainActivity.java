023d34b7f8f3034672005b76c8248e2ed7de32b2
JonathanAaron <jonathan.sandusky@gmail.com>
2013-07-29 13:12:18 -0400

    Fixed Horizontal Scrolling problem with editText.  It was talking up multiple lines.
    Also moved all elements up on the screen so if the user needs to type in
    a url they don't need to move the on screen keyboard out of the way.

--- GetKML/src/com/example/getkml/MainActivity.java
+++ GetKML/src/com/example/getkml/MainActivity.java
@@ -1,5 +1,14 @@
+/*
+ * Get KML APP
+ * Jonathan Sandusky
+ * For Ohio STEM Project
+ * 07/29/2013
+ * This Android application allows the user to selected from 2 different hard coded url address
+ * and the option to manually enter a URL to retrieve a kml or any other file Android can handle
+ * natively.
+ */
 package com.example.getkml;
-//GETKMLAPP
+
 import java.util.Locale;
 
 import android.net.Uri;
@@ -11,15 +20,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
-
 public class MainActivity extends Activity {
 // Extra Message for the .putExtra
 	public final static String EXTRA_MESSAGE = "com.example.getkml.MESSAGE";
-	String strFinalURL = "";
 	String strServer1URL = "http://kml-samples.googlecode.com/svn/trunk/kml/Region/polygon-simple.kml"; // Enter in first server site (include full address http://www.site.com/file.kml)
 	String strServer2URL = "http://kml-samples.googlecode.com/svn/trunk/kml/Region/polygon-simple.kml"; // Enter in second server site (include full address http://www.site.com/file.kml)
-	
-	//String strURL = "";
+	String strFinalURL = strServer1URL; //This makes Server 1 Default. Else if the user just pressed Send without hitting any radio buttons it the search would be empty.	
 	
 	//Called when radiobuttons are selected
 	public void onRadioButtonClicked(View view){
@@ -43,25 +49,15 @@ public class MainActivity extends Activity {
 				if (checked)
 					//use Server2
 					strFinalURL = editText.getText().toString();;
-				break;
-					
+				break;				
 		}
 	}
 	//Called when the user clicks the Send Button
 	public void displayKML(View view){
 		// Do something in response to button
-		//Intent intent = new Intent(this, GetKML.class);
-		//EditText editText = (EditText) findViewById(R.id.editText1);
-		//String strURL = editText.getText().toString();
-		//String strServer1URL = "http://kml-samples.googlecode.com/svn/trunk/kml/Region/polygon-simple.kml"; // Enter in first server site (include full address http://www.site.com/file.kml)
-		//String strServer2URL = "http://kml-samples.googlecode.com/svn/trunk/kml/Region/polygon-simple.kml"; // Enter in second server site (include full address http://www.site.com/file.kml)
-		//String strFinalURL = "";
-		//String kmlWebAddress = "http://kml-samples.googlecode.com/svn/trunk/kml/Region/polygon-simple.kml";
 		String uri = String.format(Locale.ENGLISH, "geo:0,0?q=%s",strFinalURL);
 		Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-		startActivity(intent);
-		
-		
+		startActivity(intent);	
 	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
